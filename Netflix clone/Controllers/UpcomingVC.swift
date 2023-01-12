c8c242041057bdde0c3ecbbd9b78403c802e4b0e
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-01-12 22:12:17 +0200

    - Add upcomingTable to UpcomingVC - Add API call to UpcomingVC

--- Netflix clone/Controllers/UpcomingVC.swift
+++ Netflix clone/Controllers/UpcomingVC.swift
@@ -10,6 +10,9 @@ import UIKit
 class UpcomingVC: UIViewController {
 
     
+    
+    private var titles = [Title]()
+    
     private let upcomingTable: UITableView = {
         let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -24,9 +27,57 @@ class UpcomingVC: UIViewController {
         title = "Upcoming"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        
+        view.addSubview(upcomingTable)
+        delegates()
+        fetchUpcomingMovies()
     }
     
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    
+    private func fetchUpcomingMovies() {
+        
+        APICAller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription )
+            }
+        }
+        
+    }
 
+}
 
+//MARK: - UITableViewDelegate & DataSource
+extension UpcomingVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell()}
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
+        
+        return cell
+    }
+    
+    
+    
+    
+    func delegates() {
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+    }
+    
 }
